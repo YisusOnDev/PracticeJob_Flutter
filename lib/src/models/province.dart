34aaf9e127c67a0e2e5ef0ddd0aa740520c53539
Jesús Téllez <jesustellez.contacto@gmail.com>
2021-12-02 19:15:29 +0100

    ProvinceService and usefull build commands added

--- lib/src/models/province.dart
+++ lib/src/models/province.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
 part 'province.g.dart';
 
@@ -11,4 +13,14 @@ class Province {
   factory Province.fromJson(Map<String, dynamic> json) =>
       _$ProvinceFromJson(json);
   Map<String, dynamic> toJson() => _$ProvinceToJson(this);
+}
+
+List<Province> provinceListFromJson(String pData) {
+  final provincesJson = jsonDecode(pData);
+  return List<Province>.from(provincesJson.map((x) => Province.fromJson(x)));
+}
+
+String provinceListToJson(List<Province> provinceList) {
+  final json = List<dynamic>.from(provinceList.map((x) => x.toJson()));
+  return jsonEncode(json);
 }
---
+++ lib/src/services/province_service.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:practicejob/src/models/province.dart';
+import 'package:practicejob/src/services/auth_service.dart';
+
+class ProvinceService {
+  final baseUrl = 'http://10.0.2.2:5000';
+  final AuthService _authService = AuthService();
+
+  Future<List<Province>> getAll() async {
+    var url = Uri.parse('$baseUrl/api/Province/GetAll');
+
+    try {
+      final response = await http.get(url, headers: {
+        HttpHeaders.contentTypeHeader: 'application/json',
+        HttpHeaders.acceptHeader: 'application/json',
+        HttpHeaders.authorizationHeader:
+            "Bearer " + await _authService.getCurrentToken(),
+      }).timeout(const Duration(seconds: 15));
+      if (response.statusCode == 200) {
+        return provinceListFromJson(response.body);
+      } else {
+        throw Exception('Request failed');
+      }
+    } on TimeoutException catch (_) {
+      throw TimeoutException('Request timeout');
+    }
+  }
+}
