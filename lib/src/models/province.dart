3cbc03ea15e647b98526d40b2216b46d3285483c
Jesús Téllez <jesustellez.contacto@gmail.com>
2021-12-09 19:00:36 +0100

    Override comparator method on province class

--- lib/src/models/province.dart
+++ lib/src/models/province.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:json_annotation/json_annotation.dart';
 part 'province.g.dart';
@@ -13,6 +14,12 @@ class Province {
   factory Province.fromJson(Map<String, dynamic> json) =>
       _$ProvinceFromJson(json);
   Map<String, dynamic> toJson() => _$ProvinceToJson(this);
+
+  @override
+  bool operator ==(Object other) => other is Province && id == other.id;
+
+  @override
+  int get hashCode => hashValues(id, id);
 }
 
 List<Province> provinceListFromJson(String pData) {
