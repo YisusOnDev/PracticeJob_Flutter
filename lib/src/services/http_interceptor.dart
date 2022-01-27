b3c5591dd651836dda46e743f3b4cc762146daaa
Jesús T <jesustellez.contacto@gmail.com>
2022-01-27 13:26:11 +0100

    fix(HttpInterceptor)

--- lib/src/services/http_interceptor.dart
+++ lib/src/services/http_interceptor.dart
@@ -2,11 +2,9 @@ import 'package:http/http.dart' as http;
 import 'package:practicejob/src/services/auth_service.dart';
 
 class AuthHttpClient extends http.BaseClient {
-  final AuthService authService = AuthService();
-
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) async {
-    final token = 'Bearer ' + await authService.getCurrentToken();
+    final token = 'Bearer ' + await AuthService().getCurrentToken();
     if (token.isNotEmpty) {
       request.headers.putIfAbsent('Authorization', () => token);
     }
